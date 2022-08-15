5f3ab536708301542912cd71c72480c1597e696c
Joe Chen <geometryolife@gmail.com>
2022-08-15 22:02:08 +0800

    add some envs

--- bash/env.bash
+++ bash/env.bash
@@ -1,1 +1,16 @@
 export EDITOR=nvim
+
+export PATH=$HOME/.local/bin:$PATH
+
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+
+export LOCALPROG=$HOME/prog
+export PATH=$PATH:$LOCALPROG/bin
+export PATH=$PATH:$LOCALPROG/node/bin
+
+export PATH=$HOME/.cargo/bin:$PATH
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
--- multi_packages/apt_packages.sh
+++ multi_packages/apt_packages.sh
@@ -5,3 +5,8 @@ mlocate # install locate
 wget
 file # Determine type of FILEs.
 yarnpkg # @See https://stackoverflow.com/questions/53471063/yarn-error-there-are-no-scenarios-must-have-at-least-one
+python3-venv
+python3-pip
+libinput-tools # What is the Wayland equivalent of xinput list? @See https://unix.stackexchange.com/questions/426944/what-is-the-wayland-equivalent-of-xinput-list
+libssl-dev # Openssl installation @See https://ma.ttias.be/could-not-find-directory-of-openssl-installation/
+libxcb1-dev # fix cannot find -lxcb for rust compile
