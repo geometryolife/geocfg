104ee4efda9a327071990b1c13e23f49df8cb653
Joe Chen <geometryolife@gmail.com>
2023-02-27 00:25:06 +0800

    update arch install script

--- misc/arch_install.sh
+++ misc/arch_install.sh
@@ -2,6 +2,10 @@
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 # Connect to the internet
+iwctl
+
+# Retrieve and filter a list of the latest Arch Linux mirrors
+reflector --country China --age 24 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
 
 # Enable network time synchronization
 timedatectl set-ntp true
@@ -21,30 +25,45 @@ mkswap /dev/nvme0n1p3
 swapon /dev/nvme0n1p3
 
 # Mount the filesystem
-mount /dev/nvme0n1p2 /min
+mount /dev/nvme0n1p2 /mnt
 
 mkdir /mnt/boot
 mount /dev/nvme0n1p1 /mnt/boot
 
 # Install essential packages
 pacman -Sy
-pacstrap -K /mnt base base-devel linux linux-firmware vim grub efibootmgr networkmanager iwd dhcpcd intel-ucode sof-firmware man
+pacstrap -K /mnt base base-devel linux linux-firmware vim grub efibootmgr networkmanager iwd dhcpcd intel-ucode sof-firmware alsa-utils pulseaudio pulseaudio-bluetooth cups man bash-completion
+# pacstrap -K /mnt linux-headers efivar
+pacstrap -K /mnt xorg wqy-microhei wqy-zenhei xf86-video-intel vulkan-intel mesa
 
 # Configure the system
 # Generate an fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot
+# arch-chroot /mnt bash -c "timedatectl set-timezone Asia/Shanghai && timedatectl set-ntp true && exit"
 arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && hwclock --systohc && sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen && locale-gen && echo 'LANG=en_US.UTF-8' > /etc/locale.conf && exit"
 
 arch-chroot /mnt bash -c "echo 'mx' > /etc/hostname && echo '127.0.0.1 localhost' >> /etc/hosts && echo '::1 localhost' >> /etc/hosts && echo '127.0.1.1 mx.localdomain mx' >> /etc/hosts && exit"
 
 arch-chroot /mnt bash -c "echo 'set root password' && passwd && useradd -m -G wheel joe && echo 'set joe password' && passwd joe && EDITOR=vim visudo && exit"
 
-arch-chroot /mnt bash -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH && grub-mkconfig -o /boot/grub/grub.cfg && exit"
+arch-chroot /mnt bash -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH && vim /etc/default/grub && grub-mkconfig -o /boot/grub/grub.cfg && exit"
 
-arch-chroot /mnt bash -c "systemctl enable NetworkManager && exit"
+arch-chroot /mnt bash -c "echo '# Laptop' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && echo 'evdev:atkbd:dmi:*' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && echo ' KEYBOARD_KEY_3a=leftctrl' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && echo ' KEYBOARD_KEY_1d=capslock' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && systemd-hwdb update && udevadm trigger && exit"
+
+arch-chroot /mnt bash -c "systemctl enable NetworkManager && systemctl enable iwd && exit"
+
+arch-chroot /mnt bash -c "echo 'export EDITOR=vim' >> /etc/skel/.bashrc && echo alias ls='ls --color=auto' >> /etc/skel/.bashrc && echo alias grep='grep --color=auto' >> /etc/skel/.bashrc && echo alias egrep='egrep --color=auto' >> /etc/skel/.bashrc && echo alias fgrep='fgrep --color=auto' >> /etc/skel/.bashrc && exit"
+arch-chroot /mnt bash -c "echo '[ ! -e ~/.dircolors ] && eval $(dircolors -p > ~/.dircolors)' >> /etc/skel/.bashrc && echo '[ -e /bin/dircolors ] && eval $(dircolors -b ~/.dircolors)' >> /etc/skel/.bashrc && cp -a /etc/skel/* ~ && exit"
+
+arch-chroot /mnt bash -c "sed -i 's/#export/export/' /etc/profile.d/freetype2.sh && exit"
 
 umount -R /mnt
 
 echo "Done!"
+
+# Fix the screen flickering. This is due to Panel Self Refresh (a power saving feature of Intel iGPUs).
+# vim /etc/default/grub
+# Modify => GRUB_CMDLINE_LINUX="i915.enable_psr=0"
+# grub-mkconfig -o /boot/grub/grub.cfg
