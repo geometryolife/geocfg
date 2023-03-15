ecdc7014fa9b2d75108cd1a308f60cc6875dfc53
Joe Chen <geometryolife@gmail.com>
2023-03-15 12:00:09 +0800

    update arch script

--- misc/arch_install.sh
+++ misc/arch_install.sh
@@ -1,12 +1,15 @@
+# Add the domain name resolution of raw.githubusercontent.com to /etc/hosts.
+# Find the server through this website: https://www.ip138.com/
 # wget https://raw.githubusercontent.com/geometryolife/geocfg/main/misc/arch_install.sh
+
 # Backup mirrorlist
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 # Connect to the internet
 # iwctl
 
 # Retrieve and filter a list of the latest Arch Linux mirrors
-reflector --country China --age 24 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+# reflector --country China --age 24 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
 
 # Enable network time synchronization
 timedatectl set-ntp true
@@ -34,7 +37,7 @@ mount /dev/nvme0n1p1 /mnt/boot
 # Install essential packages
 pacman -Sy
 pacstrap -K /mnt base base-devel linux linux-firmware vim neovim grub efibootmgr networkmanager iwd dhcpcd sof-firmware alsa-utils pulseaudio pulseaudio-bluetooth cups man bash-completion git wget lazygit git-delta ranger neofetch
-pacstrap -K /mnt xorg wqy-microhei wqy-zenhei firefox
+pacstrap -K /mnt xorg xorg-xinit feh udisks2 udiskie pcmanfm wqy-microhei wqy-zenhei firefox archlinux-wallpaper
 # pacstrap -K /mnt linux-headers efivar
 
 # For Intel
@@ -46,7 +49,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot
 # arch-chroot /mnt bash -c "timedatectl set-timezone Asia/Shanghai && timedatectl set-ntp true && exit"
-arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && hwclock --systohc && sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen && locale-gen && echo 'LANG=en_US.UTF-8' > /etc/locale.conf && timedatectl set-ntp true && exit"
+arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && hwclock --systohc && sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen && locale-gen && echo 'LANG=en_US.UTF-8' > /etc/locale.conf && exit"
 
 # arch-chroot /mnt bash -c "echo 'mx' > /etc/hostname && echo '127.0.0.1 localhost' >> /etc/hosts && echo '::1 localhost' >> /etc/hosts && echo '127.0.1.1 mx.localdomain mx' >> /etc/hosts && exit"
 arch-chroot /mnt bash -c "echo 'mx' > /etc/hostname && echo -e '127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tmx.localdomain\tmx' >> /etc/hosts && exit"
@@ -57,7 +60,7 @@ arch-chroot /mnt bash -c "grub-install --target=x86_64-efi --efi-directory=/boot
 
 arch-chroot /mnt bash -c "echo '# Laptop' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && echo 'evdev:atkbd:dmi:*' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && echo ' KEYBOARD_KEY_3a=leftctrl' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && echo ' KEYBOARD_KEY_1d=capslock' >> /etc/udev/hwdb.d/99-personal-kbd.hwdb && systemd-hwdb update && udevadm trigger && exit"
 
-arch-chroot /mnt bash -c "systemctl enable NetworkManager && systemctl enable iwd && systemctl enable dhcpcd && exit"
+arch-chroot /mnt bash -c "systemctl enable NetworkManager && systemctl enable iwd && systemctl enable dhcpcd && systemctl enable udisks2 && exit"
 
 # arch-chroot /mnt bash -c "echo 'export EDITOR=vim' >> /etc/skel/.bashrc && echo alias ls='ls --color=auto' >> /etc/skel/.bashrc && echo alias grep='grep --color=auto' >> /etc/skel/.bashrc && echo alias egrep='egrep --color=auto' >> /etc/skel/.bashrc && echo alias fgrep='fgrep --color=auto' >> /etc/skel/.bashrc && exit"
 # arch-chroot /mnt bash -c "echo '[ ! -e ~/.dircolors ] && eval $(dircolors -p > ~/.dircolors)' >> /etc/skel/.bashrc && echo '[ -e /bin/dircolors ] && eval $(dircolors -b ~/.dircolors)' >> /etc/skel/.bashrc && cp -a /etc/skel/* ~ && exit"
@@ -77,3 +80,7 @@ echo "Done!"
 # Clash
 # wget https://github.com/Dreamacro/clash/releases/download/v1.13.0/clash-linux-amd64-v1.13.0.gz
 # mkdir ~/.config/clash && cd clash && wget -O config.yaml "https://api.sub-100.one/link/gTWyw9hfALzBxzlD?clash=3"
+
+# Normal user
+# cp /etc/X11/xinit/xinitrc ~/.xinitrc
+# Modify => exec dwm
