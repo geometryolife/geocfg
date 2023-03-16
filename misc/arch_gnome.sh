cc25948634d32bfb6ae8b7b3a62f5be8e61de055
Joe Chen <geometryolife@gmail.com>
2023-03-16 11:00:55 +0800

    add some configs

---
+++ misc/arch_gnome.sh
@@ -0,0 +1,14 @@
+# DE
+bash -c "gnome gnome-extra"
+
+# Theme
+bash -c "arc-gtk-theme"
+
+# GNOME Shell browser integration Installation Guide https://extensions.gnome.org
+# paru gnome-browser-connector
+# Extensions
+# Dash to Dock
+# Blur my Shell
+# ArcMenu
+# Vitals
+# Just Perfection
