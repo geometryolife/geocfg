89b4a9dc456157312ca79dfa51d8273f07f7a2f6
Joe Chen <geometryolife@gmail.com>
2024-02-23 01:50:04 +0800

    [nixos] add breeze icons

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -23,6 +23,7 @@
   # environment.
   home.packages = [
     pkgs.htop
+    pkgs.breeze-icons
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
