19a95bb9f58c6870d7d2759dd5dc74ee26d57410
Joe Chen <geometryolife@gmail.com>
2024-10-02 00:13:51 +0800

    [zsh] add some aliases and envs

---
+++ zsh/aliases.zsh
@@ -0,0 +1,3 @@
+alias i='cd $HOME/i'
+alias o='cd ~/other'
+alias p='cd ~/prog'
---
+++ zsh/env.zsh
@@ -0,0 +1,5 @@
+export LOCALPROG=$HOME/prog
+export PATH=$PATH:$LOCALPROG/go/bin
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
