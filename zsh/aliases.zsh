63d179a8938f989ea6fddc13e1eac90cadbf0fcd
Joe Chen <geometryolife@gmail.com>
2024-10-03 09:51:40 +0800

    [zsh] add some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,11 @@
 alias i='cd $HOME/i'
 alias o='cd ~/other'
 alias p='cd ~/prog'
+
+alias jo='joshuto'
+alias lg='lazygit'
+alias n='neofetch'
+
+# Zellij
+alias z='zellij'
+alias za='zellij attach'
